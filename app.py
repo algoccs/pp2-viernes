50a93f71c3bab4ecc41ed7556eb5512db36d56d6
algoccs <algodemoacc@gmail.com>
2026-06-12 17:21:35 -0400

    Implement answer checking and session management
    Added answer checking functionality and session tracking.

--- app.py
+++ app.py
@@ -8,9 +8,20 @@ app.config['SECRET_KEY'] = 'NoTieneClave'
 def start_quiz(quiz_id):
     session['quiz'] = quiz_id
     session['prev_question'] = 0
+    session['totals'] = 0
+    session['corrects'] = 0
 
 def end_quiz():
     session.clear()
+
+def check_answer():
+    user_answer = request.form.get('ans_text')
+    correct_answer = session['prev_correct_ans']
+
+    if user_answer:
+        session['totals'] += 1
+        if user_answer == correct_answer:
+            session['corrects'] += 1
 
 
 def index():
@@ -25,13 +36,24 @@ def index():
         return redirect(url_for('test'))
 
 def test():
+    if 'quiz' not in session:
+        return redirect.url_for('index')
+
+    if request.method == 'POST':
+        check_answer()
+
     result = get_next_question(session['prev_question'], session['quiz'])
+
     if result is None or result == 0:
         return redirect(url_for('result'))
-    else:
-        session['prev_question'] = result[0]
 
-    return f'<h1>{result}</h1>'
+    session['prev_question'] = result[0]
+    session['prev_correct_ans'] = result[2] # RESPUESTA CORRECTA
+
+    question = result[1]
+    options = list(result[2:6])
+
+    return render_template('test.html', pregunta=question, opciones=options)
 
     
 def result():
@@ -39,7 +61,7 @@ def result():
 
 
 app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
-app.add_url_rule('/test', 'test', test)
+app.add_url_rule('/test', 'test', test, methods=['GET', 'POST'])
 app.add_url_rule('/result', 'result', result)
 
 
